a9410590cd282ec801c5435e157ea0365d0bc81f
dgw <shijiucha@qq.com>
2014-07-08 23:00:53 +0800

    dgw：Theory study。

---
+++ Study.NUnit/TheoryStudy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Study.NUnit
+{
+    public class TheoryStudy
+    {
+        [Datapoints]
+        public int[] IntValues = new int[] { -1, 0, 1, 2, 3 };
+
+        [Datapoints]
+        public string[] StringValues = 
+                                new string[] { null, string.Empty, "段光伟", "李妞妞" };
+
+        [Theory]
+        public void Assume_Study(int x, string y)
+        {
+            Assume.That(x > 0);
+            Assume.That(!string.IsNullOrEmpty(y));
+
+            Assert.IsTrue(x > 0);
+            Assert.IsTrue(!string.IsNullOrEmpty(y));
+        }
+    }
+}
